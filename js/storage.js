d8a97d52cfc9d72cf0533a69ff20407d267b5286
clewis-spec <clewis@hiveway.com>
2026-07-09 17:49:26 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,24 @@
+window.HAM = window.HAM || {};
 
+HAM.assets = JSON.parse(localStorage.getItem("hivewayAssets")) || [];
+
+HAM.assetCatalog =
+  JSON.parse(localStorage.getItem("hivewayAssetCatalog")) ||
+  structuredClone(HAM.defaultAssetCatalog);
+
+HAM.saveAssets = function () {
+  localStorage.setItem("hivewayAssets", JSON.stringify(HAM.assets));
+};
+
+HAM.saveCatalog = function () {
+  localStorage.setItem("hivewayAssetCatalog", JSON.stringify(HAM.assetCatalog));
+};
+
+HAM.downloadFile = function (filename, content) {
+  const blob = new Blob([content], { type: "text/plain" });
+  const link = document.createElement("a");
+
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+};
