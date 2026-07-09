eaa0817c1a57f059645bfc1386c3136d0f741923
clewis-spec <clewis@hiveway.com>
2026-07-09 17:51:58 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+window.HAM = window.HAM || {};
 
+HAM.initializeApp = function () {
+  HAM.initTabs();
+  HAM.initCatalog();
+  HAM.initInventory();
+  HAM.initTransfer();
+
+  HAM.updateModelOptions();
+  HAM.renderAssets();
+  HAM.updateTransferOptions();
+  HAM.renderCatalogManager();
+};
+
+document.addEventListener("DOMContentLoaded", HAM.initializeApp);
